0ae698ac29cca7111e427bed21bbe69c9de43f76
Mykola Konyk <mykola@konyk.org>
2015-11-27 23:30:51 -0500

    Adding infrastructure files.

---
+++ mix.exs
@@ -0,0 +1,33 @@
+defmodule Geo2DConvexHull.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :geo_2d_convex_hull,
+      version: "0.0.1",
+      elixir: "~> 1.0",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps
+    ]
+  end
+
+  def application do
+    [applications: []]
+  end
+
+  defp deps do
+    [
+      { :geo_2d_types, github: "ttvd/elixir-geo-2d-types"}
+    ]
+  end
+
+  def package do
+    [
+      files: ["lib", "mix.exs", "README.md"],
+      contributors: ["Mykola Konyk"],
+      licenses: ["MS-RL"],
+      links: %{"GitHub" => "https://github.com/ttvd/elixir-geo-2d-convex-hull"}
+    ]
+  end
+end
