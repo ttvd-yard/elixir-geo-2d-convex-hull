8c044eefa6e5a9e2a98888ab85daa4ff30fe2476
Mykola Konyk <mykola@konyk.org>
2015-11-28 00:34:46 -0500

    lib: convex_hull: adding main file.

---
+++ lib/convex_hull.ex
@@ -0,0 +1,14 @@
+defmodule Geo2DConvexHull do
+  require Geo2DTypes.Point2
+  #alias Geo2DTypes.Point2, as: point2
+
+  @spec compute_points([Geo2DTypes.Point2]) :: [Geo2DTypes.Point2]
+  def compute_points(points) do
+    []
+  end
+
+  @spec compute_indices([Geo2DTypes.Point2]) :: [Geo2DTypes.Point2]
+  def compute_indices(points) do
+    []
+  end
+end
